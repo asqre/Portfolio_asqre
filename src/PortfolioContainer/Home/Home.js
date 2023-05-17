73e5dae9f2160e56bd4e1d7a5b20a82e7a943a66
asqre <amita.ic.19@nitj.ac.in>
2023-05-18 03:16:07 +0530

    push code to github

---
+++ src/PortfolioContainer/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from './Profile/Profile'
+
+import './Home.css';
+export default function Home() {
+  return (
+    <div className='home-container'>
+      <Profile/>
+      <footer/>
+    
+    </div>
+  )
+}
